38d928a13891e6f9cc3793f7d224fbbacf8b0d12
Clownatique <nestor.frtr.vrgnd@gmail.com>
2025-11-11 20:07:28 +0100

    synthèse règle morpion avancé + gitignore

---
+++ rendu/peuplement.sql
@@ -0,0 +1,2 @@
+INSERT INTO morpion () VALUES(1, 'miel de Provence', 'lavande', 'IGP', 2005);
+
--- rendu/schema.sql
+++ rendu/schema.sql
@@ -69,34 +69,4 @@ CREATE TABLE equipe(
 );
 
 
-INSERT INTO Miels VALUES(1, 'miel de Provence', 'lavande', 'IGP', 2005);
-INSERT INTO Miels VALUES(2, 'miel de Provence', 'fleurs', 'IGP', 2005);
-INSERT INTO Miels VALUES(3, 'miel des Vosges', 'sapin', 'AOC', 1996);
-INSERT INTO Miels VALUES(4, 'miel d''Alsace', 'fleurs', 'IGP', 2005);
-INSERT INTO Miels VALUES(5, 'miel d''Alsace', 'forêt', 'IGP', 2005);
-INSERT INTO Miels VALUES(6, 'miel de Corse', 'maquis', 'AOC', NULL);
-INSERT INTO Miels VALUES(7, 'mel de Galicia', 'fleurs', 'IGP', 2007);
-INSERT INTO Abeilles VALUES(1, 'abeille noire', NULL, 1758);
-INSERT INTO Abeilles VALUES(2, 'abeille jaune', 'Italie', 1806);
-INSERT INTO Abeilles VALUES(3, 'abeille asiatique', 'Inde', 1793);
-INSERT INTO Abeilles VALUES(4, 'abeille russe', 'Russie', NULL);
-INSERT INTO Abeilles VALUES(5, 'abeille carniolienne', 'Slovénie', 1879);
-INSERT INTO Abeilles VALUES(6, 'abeille ibérique', 'Portugal', 1999);
-INSERT INTO Abeilles VALUES(7, 'abeille tueuse', 'Brésil', 1956);
-INSERT INTO Produire VALUES(1, 1, 'été');
-INSERT INTO Produire VALUES(1, 2, 'été');
-INSERT INTO Produire VALUES(2, 3, 'été');
-INSERT INTO Produire VALUES(3, 1, 'printemps');
-INSERT INTO Produire VALUES(3, 3, 'printemps');
-INSERT INTO Produire VALUES(3, 5, 'été');
-INSERT INTO Produire VALUES(3, 7, 'été');
-INSERT INTO Produire VALUES(4, 2, 'été');
-INSERT INTO Produire VALUES(4, 3, 'été');
-INSERT INTO Produire VALUES(4, 6, 'été');
-INSERT INTO Produire VALUES(5, 1, 'été');
-INSERT INTO Produire VALUES(5, 5, 'printemps');
-INSERT INTO Produire VALUES(6, 5, 'été');
-INSERT INTO Produire VALUES(6, 6, 'automne');
-INSERT INTO Produire VALUES(6, 7, 'printemps');
-
 
