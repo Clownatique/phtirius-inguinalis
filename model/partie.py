ab971694e83bfa0a5e0dc2d86f36d2b05a2d0c14
Flora2202 <floradubois1@gmail.com>
2025-11-17 15:32:54 +0100

    Add function to create a game in the database

--- model/partie.py
+++ model/partie.py
@@ -1,1 +1,12 @@
+import time
 
+def creer_partie(idEquipe1, idEquipe2):
+  connexion=
+  cur=connexion.cursor()
+  sql = """INSERT INTO PARTIE (idEquipe1, idEquipe2, date_debut) 
+          VALUES (%s, %s, %s) 
+          RETURNING idPartie;"""
+  valeurs=(idEquipe1, idEquipe2, datetime.now())
+  cur.execute(sql,valeurs)
+           
+  
