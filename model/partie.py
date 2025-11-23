20c5ab0deeca616442e5f1ff34bb5c459f259edf
Clownatique <nestor.frtr.vrgnd@gmail.com>
2025-11-23 18:31:54 +0100

    ajout des fonctions nécessaire au controleurs (non testé)

--- model/partie.py
+++ model/partie.py
@@ -2,57 +2,46 @@ from psycopg import sql
 from datetime import datetime
 from .utils import other_query, select_query
 
-def liste_equipe(connexion):
-  None
-
 def recompiler_partie(connexion, partie:list):
   """
     Renvoie un tableau correspondant à la partie passé en paramètre
   """
   sel_partie = """WHERE nomE1 = '%s', couleurE1 = '%s', nomE2 = '%s', couleurE2 = '%s', date = %s"""
   partie_args = [partie[0],partie[1],partie[2],partie[3],partie[4]]
-
-  query = """SELECT action FROM journal ORDER BY action DESC""" + sel_partie
+  query = """SELECT * FROM journal ORDER BY action DESC""" + sel_partie
   actions = select_query(connexion,query, partie_args)
-
   query = """SELECT taille_grille FROM partie"""+sel_partie
   taille_grille = select_query(connexion, query,partie_args)
 
-  partie = [[0 for _ in range(taille_grille)] for _ in range(taille_grille)]
+  grille = [[None for _ in range(taille_grille)] for _ in range(taille_grille)]
 
   for i in actions:
-    case = i
+    # une action dans le contexte d'une partie simple ça ressemble à ça:
+    # (0 ou 1): x,y
+    # pour l'instant les grilles ne peuvent pas aller au dessus de 9
+    x = i[2:-2]
+    y = i[4:]
+    grille[x][y] = i[:1]
+
+  return grille
+
+def verifier_action(connexion, grille:list, action):
+  # coup illégal : placer un pion alors qu'il y'en a déjà un
+  x = action[2:-2]
+  y = action[4:]
+  if partie['grille'][x][y]:
+    return False
+  return True
+
+def inserer_action(connexion, idPartie:int, action):
+  requete = """INSERT INTO JOURNAL (idP, texte_action,date_action) VALUES (%s,%s,NOW())"""
+  valeurs = [idPartie,action]
+  return other_query(requete, valeurs)
 
 
-def verifier_action(connexion, partie, action):
-  None
-
-def update_possesion(connexion, equipe, morpion):
-  None
-
-def inserer_action(connexion, partie, action):
-  None
-
-
-
-def creer_partie(connexion, nomE1, couleurE1, nomE2, couleurE2):
-  try :
-    cur=connexion.cursor()
+def creer_partie(connexion, nomE1, couleurE1, nomE2, couleurE2,est_special):
     sql = """INSERT INTO PARTIE (couleurE1, nomE1, couleurE2, nomE2, date_debut, est_special)
-          VALUES (%s, %s, %s, %s, %s, %s)
-          RETURNING idP;"""
+          VALUES (%s, %s, %s, %s, %s, %s) RETURNING idP;"""
     valeurs=(couleurE1, nomE1, couleurE2, nomE2, datetime.now(), est_special)
-    cur.execute(sql,valeurs)
-    idP=cur.fetchone()[0]
-    connexion.close()
-    cur.close()
+    idP = other_query(connexion, sql, valeurs)
     return idP
-  except psycopg.Error as e:
-    return e
-
-def creer_partie(connexion,nomE1,couleurE1,nomE2,couleurE2):
-  query = """"""
-  query = """INSERT INTO PARTIE values (couleurE1, nomE1, couleurE2, nomE2, date_debut, est_special)
-          VALUES (%s, %s, %s, %s, %s, %s)
-          RETURNING idP;"""
-  valeurs = [couleurE1, nomE1, couleurE2]
