04bd78b00783d5818fe48e91df4db133d66eb93b
Clownatique <nestor.frtr.vrgnd@gmail.com>
2025-12-01 16:35:18 +0100

    refactor(partie):split complique partie

---
+++ controleurs/partie/complique.py
@@ -0,0 +1,30 @@
+grille = [[{
+    'nom':'morpion_2',
+    'image':'t1.png',
+    'mana':12,
+    'pv':7,
+    'reu':9,
+    'atk':4
+},{},{}],[{
+    'nom':'morpion_1',
+    'image':'t3.png',
+    'mana':13,
+    'pv':5,
+    'reu':11,
+    'atk':1
+},{},{}],[{},{},{}]]
+
+REQUEST_VARS['partie']={
+    'nomE1': 'Tigers',
+    'couleurE1': 'ececec',
+    'nomE2': 'Dragons',
+    'couleurE2': 'dadada',
+    'idP': '903843bd-2f39-4a82-adb3-13bd99c1f932',
+    'grille':grille
+    'tour': 2,
+    'taille': 3,
+    'est_speciale': True
+}
+
+if POST != {}:
+    REQUEST_VARS['action_speciale'] = True
