3d5e5a25e1c99618608850469b6af3dd248ab8ed
Flora2202 <floradubois1@gmail.com>
2025-12-01 10:09:59 +0100

    Refactor equipe.py for consistent naming conventions
    j'ai juste modifié les nome, couleure en nomE, couleurE c'etait des mini erreurs de syntaxes +modification de la fonction insertion_equipe pour la mettre sous format try et except

--- model/equipe.py
+++ model/equipe.py
@@ -3,21 +3,21 @@ from model.utils import select_query, other_query,get_instances,count_instances,
 from datetime import datetime
 
 def noms_utilises(connexion):
-    query =  'SELECT nome FROM equipe'
+    query =  'SELECT nomE FROM equipe'
     return select_query(connexion, query)
 
 def liste_equipes(connexion):
-    query =  'SELECT nome, couleure FROM equipe'
+    query =  'SELECT nomE, couleurE, date_creation FROM equipe'
     return select_query(connexion, query)
 
 def liste_morpion_une_equipe(connexion,nom):
-    requete = '''SELECT image,m.pv,m.atk,m.mana,m.reu from morpion m join
+    requete = '''SELECT m.image, m.pv, m.atk, m.mana, m.reu from morpion m join
     posseder p using(idm) where p.nomE = %s '''
     params = [nom]
     return select_query(connexion, requete, params)
 
 def couleur_prises(connexion):
-    requete = 'SELECT couleure FROM equipe'
+    requete = 'SELECT couleurE FROM equipe'
     return select_query(connexion, requete)
 
 def liste_morpion(connexion):
@@ -25,20 +25,26 @@ def liste_morpion(connexion):
     return select_query(connexion, requete)
 
 def insertion_equipe(connexion, nom, couleur):
-    query = f"""INSERT INTO equipe (nome, couleure, date_creation) VALUES(%s,%s,%s)"""
-    return other_query(connexion, query, [nom[0], couleur[0], datetime.now()])
+    try :
+        query = f"""INSERT INTO equipe (nomE, couleurE, date_creation) VALUES(%s,%s,%s)"""
+        return other_query(connexion, query, [nom, couleur, datetime.now()])
+    except psycopg.IntegrityError :
+        raise ValueError(f"Le nom '{nom}' ou la couleur '{couleur}' est déjà utilisé")
 
 def insertion_posseder(connexion, nom, couleur, liste_morpion):
     """
     Prends un tableau d'indice de morpion
     Renvoie le nombre de morpions insérés
     """
+    nb_inseres=0
     for indice_morpion in liste_morpion:
         # faire le traitement pour récupérer les stats
         # de base du morpion pour les affecter
-        query ="INSERT INTO posseder values (%s,%s)"
-        other_query(connexion, query,[int(indice_morpion), nom[0]])
+        query ="INSERT INTO posseder (idM, nomE) values (%s,%s)"
+        other_query(connexion, query,[int(indice_morpion), nom])
+        nb_inseres+=1
+    return nb_inseres
 
 def supprimer_equipe(connexion, nom):
-    query = f"""DELETE FROM Equipe WHERE nome = %s"""
-    other_query(connexion,query, [nom])
+    query = """DELETE FROM Equipe WHERE nomE = %s"""
+    return other_query(connexion,query, [nom])
